8f6601f07d16db1e212d758a015560e33a819e51
浩哥 <2051672879@qq.com>
2017-03-02 11:53:18 +0800

    Laravel Collection sum 求价格总和重构方法

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -23,12 +23,41 @@ class IndexController extends Controller
 
 
         /* 求价格总和 传统 foreach循环相加 做法 */
-        $price =0;
+        /*$price =0;
         foreach ($oreders as $oreder ) {
             foreach ($oreder['order_products'] as $order_product){
                 $price += $order_product['price'];
             }
         }
-        dd($price);
+        dd($price);*/
+
+
+        /* 1、Laravel Collection sum 求价格总和 */
+        /*dump(collect($oreders)->map(function ($order){
+            return $order['order_products'];
+        })->flatten(1)->map(function ($order){
+            return $order['price'];
+        })->sum());*/
+
+
+        /* 2、Laravel Collection sum 求价格总和 */
+        /*dump(collect($oreders)->flatMap(function ($order){
+            return $order['order_products'];
+        })->map(function ($order){
+            return $order['price'];
+        })->sum());*/
+
+
+        /* 3、Laravel Collection sum 求价格总和 */
+        /*dump(collect($oreders)->flatMap(function ($order){
+            return $order['order_products'];
+        })->pluck('price')->sum());*/
+
+
+        /* 4、Laravel Collection sum 求价格总和 */
+        dump(collect($oreders)->flatMap(function ($order){
+            return $order['order_products'];
+        })->sum('price'));
+
     }
 }
